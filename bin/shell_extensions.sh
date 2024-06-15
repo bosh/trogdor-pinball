4da0670438ad24e6fe527480b67220f8b288950b
Alex Lobascio <alex@Lobasc.io>
2024-06-14 23:39:31 -0700

    add shell extensions for faster machine setup

---
+++ bin/shell_extensions.sh
@@ -0,0 +1,7 @@
+alias w='cd ~/workspace/'
+alias t='cd ~/workspace/trogdor-pinball'
+alias m='cd ~/workspace/mpf'
+alias md="cd ~/workspace/mpf-docs"
+alias e=explorer
+alias o=open
+alias mpfpenv=". ~/mpf80env/bin/activate"
