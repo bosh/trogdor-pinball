1929f4a3f6227d89a10f11917f1551d87e64676a
Alex Lobascio <alex@Lobasc.io>
2024-06-14 23:42:04 -0700

    add shell one time setup commands for ease of use

---
+++ bin/shell_one_time_setup.sh
@@ -0,0 +1,9 @@
+git config --global core.editor 'vim'
+git config --global user.name 'Alex Lobascio'
+git config --global user.email 'alex@lobasc.io'
+git config --global alias.w whatchanged
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
