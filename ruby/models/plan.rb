f88e8c322877df268121b12a9fb8ca3d4b43a3d6
Alex Lobascio <alex@lobasc.io>
2024-09-20 03:33:53 -0700

    reenable all the other generated and future configs for smart_virtual platform
    also add number generation and default smart_virtual platform tagging to generated lights

--- ruby/models/plan.rb
+++ ruby/models/plan.rb
@@ -3,13 +3,15 @@ module TrogBuild
     attr_reader :modes
 
     def initialize()
+      @next_light_number = 11001
       @lights = {}
       @modes = []
     end
 
     def add_light(name, config = nil)
       config ||= {}
-      config['number'] = nil unless config['number']
+      config['number'] = consume_light_number unless config['number']
+      config['platform'] = 'smart_virtual' unless config['smart_virtual']
       config['type'] = 'rgb' unless config['type']
       raise "Light #{name} already registered" if @lights[name]
       @lights[name] = config
@@ -17,6 +19,12 @@ module TrogBuild
 
     def add_mode(mode)
       @modes << mode
+    end
+
+    def consume_light_number
+      next_number = @next_light_number
+      @next_light_number += 1
+      next_number
     end
 
     def lights
