707931f8c140c9ab23a54da1274f77c6348c832b
Alex Lobascio <alex@Lobasc.io>
2024-12-06 00:29:06 -0800

    move two right cbg controlled ball gate diverter definitions to ruby generation

--- ruby/game/diverters.rb
+++ ruby/game/diverters.rb
@@ -1,18 +1,42 @@
 module TrogBuild
   class DivertersConfig < ConfigFile
-    DIVERTERS = [
-    ]
+    DIVERTERS = {
+        'div_right_controlled_ball_gate' => {
+          'activation_coil' => 'c_gate_right',
+          'type' => 'hold',
+          'activation_time' => '3s',
+          'enable_events' => 'enable_lift_right_cbg',
+          'disable_events' => 'disable_lift_right_cbg, ball_ending',
+          'activation_switches' => 's_left_orbit',
+          'activate_events' => 'activate_right_cbg',
+          'deactivate_events' => 'deactivate_right_cbg, disable_lift_right_cbg, ball_ending'
+        },
+        'div_right_controlled_ball_gate_alternative_profile' => {
+          'activation_coil' => 'c_gate_right',
+          'type' => 'hold',
+          'activation_time' => '5s',
+          'enable_events' => 'enable_lift_right_cbg_alt',
+          'disable_events' => 'disable_lift_right_cbg_alt, ball_ending',
+          'activation_switches' => 's_left_orbit, s_start', #Allow start as well for user control
+          'activate_events' => 'activate_right_cbg_alt',
+          'deactivate_events' => 'deactivate_right_cbg_alt, disable_lift_right_cbg_alt, ball_ending'
+        }
+      }
 
     def to_hash
       diverter_definitions = {}
-      # DIVERTERS.each { |d| add_diverter_combinations(diverter_definitions, d) }
+      DIVERTERS.each { |name, definition| add_diverter_combinations(diverter_definitions, name, definition) }
       {'diverters' => diverter_definitions}
     end
 
     private
 
-    def add_diverter_combinations(collection, name)
-
+    def add_diverter_combinations(collection, name, definition)
+      if collection[name]
+        puts "Duplicate diverter definition detected - #{name}"
+        exit(1)
+      end
+      collection[name] = definition
     end
   end
 end
