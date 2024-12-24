b5fb9e41968958febe79615b261ac96156cf6d51
Alex Lobascio <alex@Lobasc.io>
2024-12-24 00:03:22 -0800

    move all diverter generation to ruby
    still needs actual refactoring of the formula in ruby
    to dry it up

--- ruby/game/diverters.rb
+++ ruby/game/diverters.rb
@@ -20,6 +20,25 @@ module TrogBuild
           'activation_switches' => 's_left_orbit, s_start', #Allow start as well for user control
           'activate_events' => 'activate_right_cbg_alt',
           'deactivate_events' => 'deactivate_right_cbg_alt, disable_lift_right_cbg_alt, ball_ending'
+        },
+        'div_left_controlled_ball_gate' => {
+          'activation_coil' => 'c_gate_left',
+          'type' => 'hold',
+          'activation_time' => '3s',
+          'enable_events' => 'enable_lift_left_cbg',
+          'disable_events' => 'disable_lift_left_cbg, ball_ending',
+          'activation_switches' => 's_right_orbit',
+          'activate_events' => 'activate_left_cbg',
+          'deactivate_events' => 'deactivate_left_cbg'
+        },
+        'div_saver_post' => {
+          'activation_coil' => 'c_saver_post',
+          'type' => 'hold',
+          'activation_time' => '10s',
+          'enable_events' => 'enable_saver_post',
+          'disable_events' => 'disable_saver_post, ball_ending',
+          'activate_events' => 'activate_saver_post',
+          'deactivate_events' => 'deactivate_saver_post, disable_saver_post, ball_ending'
         }
       }
 
