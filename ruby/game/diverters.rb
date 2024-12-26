8675f60ce4f81d2798b2c8501c1c06f6f69905ba
Alex Lobascio <alex@Lobasc.io>
2024-12-26 02:11:23 -0800

    add after_initialize hook for config_file and refactor diverter generation to hook in
    got rid of abbreviated names (cbg) for diverters to keep them in line with the standard definition

--- ruby/game/diverters.rb
+++ ruby/game/diverters.rb
@@ -1,54 +1,36 @@
 module TrogBuild
   class DivertersConfig < ConfigFile
-    DIVERTERS = {
-        'div_right_controlled_ball_gate' => {
-          'activation_coil' => 'c_gate_right',
-          'type' => 'hold',
-          'activation_time' => '3s',
-          'enable_events' => 'enable_lift_right_cbg',
-          'disable_events' => 'disable_lift_right_cbg, ball_ending',
-          'activation_switches' => 's_left_orbit',
-          'activate_events' => 'activate_right_cbg',
-          'deactivate_events' => 'deactivate_right_cbg, disable_lift_right_cbg, ball_ending'
-        },
-        'div_right_controlled_ball_gate_alternative_profile' => {
-          'activation_coil' => 'c_gate_right',
-          'type' => 'hold',
-          'activation_time' => '5s',
-          'enable_events' => 'enable_lift_right_cbg_alt',
-          'disable_events' => 'disable_lift_right_cbg_alt, ball_ending',
-          'activation_switches' => 's_left_orbit, s_start', #Allow start as well for user control
-          'activate_events' => 'activate_right_cbg_alt',
-          'deactivate_events' => 'deactivate_right_cbg_alt, disable_lift_right_cbg_alt, ball_ending'
-        },
-        'div_left_controlled_ball_gate' => {
-          'activation_coil' => 'c_gate_left',
-          'type' => 'hold',
-          'activation_time' => '3s',
-          'enable_events' => 'enable_lift_left_cbg',
-          'disable_events' => 'disable_lift_left_cbg, ball_ending',
-          'activation_switches' => 's_right_orbit',
-          'activate_events' => 'activate_left_cbg',
-          'deactivate_events' => 'deactivate_left_cbg'
-        },
-        'div_saver_post' => {
-          'activation_coil' => 'c_saver_post',
-          'type' => 'hold',
-          'activation_time' => '10s',
-          'enable_events' => 'enable_saver_post',
-          'disable_events' => 'disable_saver_post, ball_ending',
-          'activate_events' => 'activate_saver_post',
-          'deactivate_events' => 'deactivate_saver_post, disable_saver_post, ball_ending'
-        }
-      }
-
     def to_hash
       diverter_definitions = {}
-      DIVERTERS.each { |name, definition| add_diverter_combinations(diverter_definitions, name, definition) }
+      @diverters.each { |name, definition| add_diverter_combinations(diverter_definitions, name, definition) }
       {'diverters' => diverter_definitions}
     end
 
     private
+
+    def after_initialize
+      @diverters = {}
+      add_diverter!('right_controlled_ball_gate', 'c_gate_right', '3s', 's_left_orbit')
+      add_diverter!('right_controlled_ball_gate_alternative_profile', 'c_gate_right', '6s', 's_left_orbit')
+      add_diverter!('left_controlled_ball_gate', 'c_gate_left', '3s', 's_right_orbit')
+      add_diverter!('saver_post', 'c_saver_post', '10s')
+    end
+
+    def add_diverter!(diverter_name, activation_coil_name, activation_time, activation_switches = nil)
+      @diverters["div_#{diverter_name}"] = diverter_default_configuration(diverter_name, activation_coil_name, activation_time)
+    end
+
+    def diverter_default_configuration(diverter_name, activation_coil_name, activation_time)
+      {
+        'activation_coil' => activation_coil_name,
+        'type' => 'hold',
+        'activation_time' => activation_time,
+        'enable_events' => "enable_#{diverter_name}",
+        'disable_events' => "disable_#{diverter_name},ball_ending",
+        'activate_events' => "activate_#{diverter_name}",
+        'deactivate_events' => "deactivate_#{diverter_name},disable_#{diverter_name},ball_ending"
+      }
+    end
 
     def add_diverter_combinations(collection, name, definition)
       if collection[name]
--- ruby/models/config_file.rb
+++ ruby/models/config_file.rb
@@ -3,6 +3,11 @@ module TrogBuild
     attr_reader :name
     def initialize(name)
       @name = name
+      after_initialize
+    end
+
+    def after_initialize
+      #Stub for subclasses
     end
   end
 end
