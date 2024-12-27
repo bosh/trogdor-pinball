efd0ebfd5c7a5d33764e2d4c8dde31df7fdd0c12
Alex Lobascio <alex@Lobasc.io>
2024-12-27 01:45:39 -0800

    improve shell extensions for usage with "both" and gmc integration

--- bin/shell_extensions.sh
+++ bin/shell_extensions.sh
@@ -2,18 +2,17 @@ alias t='cd ~/workspace/trogdor-pinball'
 alias m='cd ~/workspace/mpf'
 alias md='cd ~/workspace/mpf-docs'
 alias mg='cd ~/workspace/mpf-gmc'
-alias mpfm='mpf monitor & mpf '
+alias mpfm='mpf monitor & mpf ' # Root call for many other calls with monitor added on
 
 #-b removes bcp dependency
 #-t changes to log mode
 #-V verbose logging
 #-x/X dumb/smart virtual interface to remove hardware dependency
-alias snake="clear; t; mpf game -bt" #log tail with no bcp connection required
-alias vsnake="tmonitor& clear; t; mpf game -btX" #log tail with no bcp connection required, virtual mode?
-alias tmonitor="t; mpf monitor"
+#-g GMC project directory
+#-G GMC executable if not on path
+alias vsnake="clear; t; mpfm game -btX" #log tail with no bcp connection required, virtual mode? # Used by windows taskbar integration
+alias vboth="clear; t; mpfm both -g gmc -tX" #requires godot executable on the path
 
-alias trogdor="clear; t; mpf game" #requires bcp connection
-alias tmc="echo TODO" #to run the mc or gmc +bcp client #probably mpf gmc ...
 alias tmedia='~/workspace/pinball-media/pinball-media.rb install trogdor'
 alias tmediax='~/workspace/pinball-media/pinball-media.rb clean trogdor'
 
