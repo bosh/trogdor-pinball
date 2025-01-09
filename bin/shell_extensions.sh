414bd4bf753461691a7b7f238b9a0a85788b9999
Alex Lobascio <alex@Lobasc.io>
2025-01-08 18:24:42 -0800

    add alias to run godot project directly
    requires godot available on the path

--- bin/shell_extensions.sh
+++ bin/shell_extensions.sh
@@ -22,3 +22,5 @@ alias tmediax='~/workspace/pinball-media/pinball-media.rb clean trogdor'
 
 alias mpftest="python -m unittest discover mpf/tests"
 alias mpfpenv=". ~/mpf80env/bin/activate"
+
+alias tplay="t; godot --path gmc/"
