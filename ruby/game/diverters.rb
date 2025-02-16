db149f341463ac1990fce522b5e55e341e4b465b
Alex Lobascio <alex@Lobasc.io>
2025-02-15 23:28:13 -0800

    update future devices with U turn switches, left pocket targets, subway items etc

--- ruby/game/diverters.rb
+++ ruby/game/diverters.rb
@@ -10,9 +10,9 @@ module TrogBuild
 
     def after_initialize
       @diverters = {}
-      add_diverter!('right_controlled_ball_gate', 'c_gate_right', '3s', 's_left_orbit')
-      add_diverter!('right_controlled_ball_gate_alternative_profile', 'c_gate_right', '6s', 's_left_orbit')
-      add_diverter!('left_controlled_ball_gate', 'c_gate_left', '3s', 's_right_orbit')
+      add_diverter!('right_controlled_ball_gate', 'c_gate_right', '3s', 's_left_orbit_outer')
+      add_diverter!('right_controlled_ball_gate_alternative_profile', 'c_gate_right', '6s', 's_left_orbit_outer')
+      add_diverter!('left_controlled_ball_gate', 'c_gate_left', '3s', 's_right_orbit_outer')
       add_diverter!('saver_post', 'c_saver_post', '10s')
       add_diverter!('right_outlane_post', 'c_right_outlane_post', '3s', 's_right_outer_lane')
       add_diverter!('right_outlane_post_ramp_save', 'c_right_outlane_post', '7s', nil, {active: 'bd_plunger', inactive: 'bd_trough'})
