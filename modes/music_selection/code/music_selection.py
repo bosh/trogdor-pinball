bc3fe3d3b1a1e72ca3c2912d49a6acaef71bcd7f
Alex Lobascio <alex@Lobasc.io>
2025-04-15 23:26:33 -0700

    add song selection carousel mode for start of ball
    had to disable some music that plays early on in ball
    - most short music should probably be effects instead or a new bus
    
    carousel items dont actually display yet, but the state machine
    picking a song to start and rotating through based on flipper
    action works.

---
+++ modes/music_selection/code/music_selection.py
@@ -0,0 +1,30 @@
+from mpf.modes.carousel.code.carousel import Carousel
+
+class MusicSelection(Carousel):
+  __slots__ = []
+
+  def mode_init(self):
+    try:
+        super().mode_init()
+    except AssertionError:
+        pass
+    self.machine.log.info("TrogPy::MusicSelection initialized")
+
+  def mode_start(self, **kwargs):
+    self.machine.log.info("TrogPy::MusicSelection starting")
+
+    if self._all_items:
+      super().mode_start(**kwargs) #Carousel base class
+    else:
+      self.machine.log.error("TrogPy::MusicSelection no items!!! X.X")
+
+    self.select_item_based_on_state_machine()
+
+  def mode_stop(self, **kwargs):
+    self.machine.log.info("TrogPy::MusicSelection stopping")
+
+  def select_item_based_on_state_machine(self):
+    initial_item = self.machine.state_machines['music_jukebox'].state
+    self._highlighted_item_index = int(initial_item.split("_")[-1]) - 1
+    self.machine.log.info(f"TrogPy::MusicSelection initial highlighting item {initial_item}")
+    self.machine.events.post('item_highlighted', carousel=self.name, direction='forward', item=initial_item)
