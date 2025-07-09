c8861232ad4c3700cdc6b748f2aadff49ba9586e
Alex Lobascio <alex@Lobasc.io>
2025-07-08 18:03:20 -0700

    animate the fire sprites on base with an extension script file

---
+++ gmc/slides/base.gd
@@ -0,0 +1,6 @@
+extends MPFSlide
+
+func _ready() -> void:
+    $"Static Assets/peasant_animation".play()
+    $"Static Assets/breath_animation".play()
+    $"Static Assets/cottage_fire_animation".play()
