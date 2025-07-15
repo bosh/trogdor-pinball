a7f6e292fef951635df65f69a2d30a1498588aa0
Alex Lobascio <alex@Lobasc.io>
2025-07-15 02:28:37 -0700

    add start timestamp and end timestamp to player vars per ball and report it and duration in seconds in analytics

--- modes/code_base/code/code_base.py
+++ modes/code_base/code/code_base.py
@@ -1,3 +1,4 @@
+import time
 from mpf.core.mode import Mode
 
 class CodeBase(Mode):
@@ -6,6 +7,10 @@ class CodeBase(Mode):
 
   def mode_start(self, **kwargs):
     self.machine.log.info("TrogPy::CodeBase starting")
+    timestamp = time.time()
+    self.machine.events.post('update_player_ball_start', timestamp=round(timestamp,2), timestamp_format=time.strftime("%Y-%m-%d %H:%M:%S"))
 
   def mode_stop(self, **kwargs):
     self.machine.log.info("TrogPy::CodeBase stopping")
+    timestamp = time.time()
+    self.machine.events.post('update_player_ball_end', timestamp=round(timestamp,2), timestamp_format=time.strftime("%Y-%m-%d %H:%M:%S"))
