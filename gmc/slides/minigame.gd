b42aff4a05c63c914a7b7deaf5a774ebd41f5ada
Alex Lobascio <alex@Lobasc.io>
2025-08-03 02:53:43 -0700

    change minigame behaviors to track facing and continuous action
    and show/hide the cheat icons to indicate continuous action for now

--- gmc/slides/minigame.gd
+++ gmc/slides/minigame.gd
@@ -2,20 +2,25 @@ extends MPFSlide
 
 @export var player_speed = 10 # pixels/second
 var screen_size # window size
+var trogdor_facing = "right"
+var continuous_action = "stop"
 
 # STANDARD NODE
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	trogdor_facing = "right"
 	$left.visible = false
 	$right.visible = false
 	$player/CollisionShape2D.disabled = false
 	$GameTimer.start()
 	$player/FlameSpriteLeft.visible = false
 	$player/FlameSpriteRight.visible = false
-	$player.start($PlayerStart.position)
+	$player.position = $PlayerStart.position
 
 func _process(delta):
+	$left.visible = continuous_action == "left"
+	$right.visible = continuous_action == "right"
 	move_player(delta)
 
 # SIGNAL BINDINGS
@@ -38,32 +43,36 @@ func _on_game_timer_timeout():
 
 func action_left(_settings, _kwargs):
 	print("Minigame Action - Left")
-	$left.visible = true
-	$right.visible = false
+	trogdor_facing = "left"
+	continuous_action = "left"
+
 
 func action_right(_settings, _kwargs):
 	print("Minigame Action - Right")
-	$left.visible = false
-	$right.visible = true
+	continuous_action = "right"
+	trogdor_facing = "right"
 
 func action_start(_settings, _kwargs):
 	print("Minigame Action - Start")
-	$left.visible = !$left.visible
-	$right.visible = !$right.visible
+	if continuous_action == "stop":
+		continuous_action = trogdor_facing
+	else:
+		continuous_action = "stop"
 
 # CUSTOM PUBLIC FNS
 
 func stop_player():
-	$left.visible = false
-	$right.visible = false
+	continuous_action = "stop"
 	$player/TrogdorSprite.stop()
 
 func move_player(delta):
-	var direction = _get_action_direction()
+	var direction = _get_continuous_velocity()
 
 	if direction < 0:
+		trogdor_facing = "left"
 		$player/TrogdorSprite.play("walk_left")
 	elif direction > 0:
+		trogdor_facing = "right"
 		$player/TrogdorSprite.play("walk_right")
 	else:
 		stop_player()
@@ -73,29 +82,28 @@ func move_player(delta):
 
 func activate_flames():
 	# TODO changing direction should swap the animation direction. Maybe position and scale should be changed instead
-	var facing = _get_action_direction()
-	if facing <= 0: # TODO this is actually different than action because frozen still has facing...
+	if trogdor_facing == "left": # TODO this is actually different than action because frozen still has facing...
 		$player/FlameSpriteLeft.visible = true
 		$player/FlameSpriteLeft.play()
-	elif facing > 0:
+	else:
 		$player/FlameSpriteRight.visible = true
 		$player/FlameSpriteRight.play()
 
 # CUSTOM PRIVATE FNS
 
-func _get_action_direction():
-	if $left.visible && !$right.visible:
+func _get_continuous_velocity():
+	if continuous_action == "left":
 		return -1
-	if $right.visible && !$left.visible:
+	if continuous_action == "right":
 		return 1
 	return 0
 
 func _check_edges():
 	var clamped_position = $player.position.clamp(Vector2.ZERO, screen_size)
 	if $player.position != clamped_position:
+		print("Minigame player hit edge")
 		$player.position = clamped_position
 		stop_player()
-		print("Minigame player hit edge")
 
 func _on_flame_sprite_right_animation_finished():
 	$player/FlameSpriteRight.visible = false
