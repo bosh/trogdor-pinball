baa0bb7ba0b15e3db29def248243a4fb138b1485
Alex Lobascio <alex@Lobasc.io>
2025-08-14 22:29:34 -0700

    minigame uses flip_h for sprite direction management and has wall collision but lost heart and coin pickup responsiveness

--- gmc/slides/minigame.gd
+++ gmc/slides/minigame.gd
@@ -21,24 +21,10 @@ func _ready():
 func _process(delta):
 	$left.visible = continuous_action == "left"
 	$right.visible = continuous_action == "right"
-	move_player(delta)
+	var direction = _get_continuous_velocity()
+	$player.move_and_collide(Vector2((direction * player_speed * delta), 0))
 
 # SIGNAL BINDINGS
-
-func _on_player_body_entered(body):
-	if body == $Heart:
-		$Heart/CollisionShape2D.set_deferred("disabled", true)
-		$Heart/HeartAnimation.play()
-		print("Heart captured")
-		activate_flames()
-		MPF.server.send_event("minigame_heart_capture")
-	if body == $Coins:
-		$Coins/CollisionShape2D.set_deferred("disabled", true)
-		print("Coins captured")
-		continuous_action = "stop"
-		$player/TrogdorSprite.play("flex")
-		$Coins.queue_free()
-		MPF.server.send_event("minigame_coin_capture")
 
 func _on_heart_animation_animation_finished():
 	$Heart.queue_free()
@@ -53,16 +39,22 @@ func action_left(_settings, _kwargs):
 	print("Minigame Action Received - Left")
 	trogdor_facing = "left"
 	continuous_action = "left"
+	$player/TrogdorSprite.flip_h = true
+	$player/TrogdorSprite.play("walk")
 
 func action_right(_settings, _kwargs):
 	print("Minigame Action Received - Right")
-	continuous_action = "right"
 	trogdor_facing = "right"
+	continuous_action = "right"
+	$player/TrogdorSprite.flip_h = false
+	$player/TrogdorSprite.play("walk")
 
 func action_start(_settings, _kwargs):
 	print("Minigame Action Received - Start")
 	if continuous_action == "stop":
 		continuous_action = trogdor_facing
+		$player/TrogdorSprite.flip_h = (trogdor_facing == "left")
+		$player/TrogdorSprite.play("walk")
 	else:
 		stop_player()
 
@@ -71,30 +63,6 @@ func action_start(_settings, _kwargs):
 func stop_player():
 	continuous_action = "stop"
 	$player/TrogdorSprite.play("idle")
-
-func move_player(delta):
-	var direction = _get_continuous_velocity()
-
-	if direction < 0:
-		trogdor_facing = "left"
-		$player/TrogdorSprite.play("walk")
-	elif direction > 0:
-		trogdor_facing = "right"
-		$player/TrogdorSprite.play("walk")
-	else:
-		pass
-		#not moving
-
-	var player = $player
-	var abs_x_scale = abs(player.scale.x)
-
-	if trogdor_facing == "left":
-		player.scale.x = - abs_x_scale
-	elif trogdor_facing == "right":
-		player.scale.x = abs_x_scale
-
-	$player.position.x = $player.position.x + (direction * player_speed * delta)
-	_check_edges()
 
 func activate_flames():
 	$player/firebreath.visible = true
@@ -109,13 +77,6 @@ func _get_continuous_velocity():
 		return 1
 	return 0
 
-func _check_edges():
-	var clamped_position = $player.position.clamp(Vector2.ZERO, screen_size)
-	if $player.position != clamped_position:
-		print("Minigame player hit edge")
-		$player.position = clamped_position
-		stop_player()
-
 func _on_flame_sprite_animation_finished():
 	$player/firebreath.visible = false
 
@@ -123,3 +84,18 @@ func _on_trogdor_sprite_animation_finished():
 	if $player/TrogdorSprite.animation == "flex":
 		$player/TrogdorSprite.play("idle")
 		activate_flames()
+
+func _on_coins_body_entered(_body):
+	$Coins/CollisionShape2D.set_deferred("disabled", true)
+	print("Coins captured")
+	continuous_action = "stop"
+	$player/TrogdorSprite.play("flex")
+	$Coins.queue_free()
+	MPF.server.send_event("minigame_coin_capture")
+
+func _on_heart_body_entered(_body):
+	$Heart/CollisionShape2D.set_deferred("disabled", true)
+	$Heart/HeartAnimation.play()
+	print("Heart captured")
+	activate_flames()
+	MPF.server.send_event("minigame_heart_capture")
